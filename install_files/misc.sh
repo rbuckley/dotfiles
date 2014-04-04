711088cde1332b8c5a06d45298c0d180bb2c2776
Robert Buckley <rbuckley30@gmail.com>
2014-04-04 14:14:09 -0700

    Added fonts for sexy airline symbols
    other changes are boring

--- install_files/misc.sh
+++ install_files/misc.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
+# use echo <something> to output to the logfile
+# use echo <something> >&3 to output to the console
 log2file() {
-    echo "Creating logfile redirects to ${LOG_FILE}..."
-    # 
     exec 3>&1 4>&2
     # on exit, hangup, interrupt or quit make sure we put the redirects back
     # to stdout and stderr
     trap 'exec 2>&4 1>&3' 0 1 2 3
     exec 1>${LOG_FILE} 2>&1
-}
-
-identify_os() {
-    echo "Your good"
 }
