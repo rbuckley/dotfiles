db0308c9e56488fbdd8d12527879232b539db89f
Robert Buckley <robert.buckley@commscope.com>
2020-04-16 09:24:24 -0700

    Begin work on new bootstrap.sh for new systems

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,19 +30,19 @@ ctrl_c() {
 
 echo '================================================================================================' >&3
 echo 'Setting up a new workstation:' >&3
-echo -n '================================================================================================' >&3
+echo '================================================================================================' >&3
 
 echo '------------------------------------------------------------------------------------------------' >&3
 echo 'Installing package dependencies:' >&3
-echo -n '------------------------------------------------------------------------------------------------' >&3
+echo '------------------------------------------------------------------------------------------------' >&3
 ## apt install everything we need to build from source
 sudo apt install build-essential curl
 
 echo '------------------------------------------------------------------------------------------------' >&3
 echo 'Initializing submodules:' >&3
 echo '------------------------------------------------------------------------------------------------' >&3
-git submodule init --progress
-git submodule update --progress
+git submodule init
+git submodule update
 
 echo '------------------------------------------------------------------------------------------------' >&3
 echo 'Installing neovim:' >&3
@@ -66,10 +66,22 @@ sudo make install
 popd
 
 mkdir ${CURDIR}/nodejs
+pushd nodejs
 NODE_VER=v12.16.2
 DISTRO=linux-x64
 sudo mkdir -p /usr/local/lib/nodejs
-curl https://nodejs.org/dist/${NODE_VER}/node-${NODE_VER}-${DISTRO}.tar.xz ${CURDIR}/nodejs
-sudo tar Jvxf ${CURDIR}/nodejs/node-${NODE_VER}-${DISTRO}.tar.xz -C /usr/local/lib/nodejs
+curl -O -L https://nodejs.org/dist/${NODE_VER}/node-${NODE_VER}-${DISTRO}.tar.xz 
+sudo tar Jvxf nodejs/node-${NODE_VER}-${DISTRO}.tar.xz -C /usr/local/lib/nodejs
+
+
+mkdir ${CURDIR}/openvpn
+pushd openvpn
+sudo apt install -y vpnc-scripts libssl-dev libxml2-dev
+curl -O -L ftp://ftp.infradead.org/pub/openconnect/openconnect-8.08.tar.gz
+tar vxzf openconnect-8.08.tar.gz
+pushd openconnect-8.08
+./configure
+make 
+sudo make install
 
 exit 0
